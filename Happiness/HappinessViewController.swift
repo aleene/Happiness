8c519fe0be37deb9c9be630a866872c905c680dd
Arnaud Leene <aleene@gmail.com>
2016-01-03 17:20:49 +0100

    This version implements the Happiness app as demonstrated in lecture 5 of the Stanford CS193p course.

---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by arnaud on 03/01/16.
+//  Copyright © 2016 Hovering Above. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+   
+}
